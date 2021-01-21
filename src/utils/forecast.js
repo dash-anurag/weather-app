daf556fe817006d8e7d88338b277028e0f1f5546
Anurag Dash <anuragjeypore1234@gmail.com>
2021-01-22 02:03:36 +0530

    added some new weather parameter

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -17,11 +17,15 @@ const forecast = (latitude, longitude, callback) => {
       callback(
         undefined,
         body.current.weather_descriptions[0] +
-          ": It is currently " +
+          ". It is currently " +
           body.current.temperature +
           " degrees out. It feels like " +
           body.current.feelslike +
-          " degrees out."
+          " degrees out. " +
+          "The humidity is " +
+          body.current.humidity + "%" +
+          " and the wind speed is " +
+          body.current.wind_speed + "km/hr."
       );
     }
   });
